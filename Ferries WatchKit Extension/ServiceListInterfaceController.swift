d84e85fb5fcfd26bdbc02656e628182b9af45b03
Stefan Church <stefan.church@gmail.com>
2015-05-23 20:03:29 +1200

    Started creating watch app.

---
+++ Ferries WatchKit Extension/ServiceListInterfaceController.swift
@@ -0,0 +1,51 @@
+//
+//  InterfaceController.swift
+//  Ferries WatchKit Extension
+//
+//  Created by Stefan Church on 23/05/15.
+//  Copyright (c) 2015 Stefan Church. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ServiceListInterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var table: WKInterfaceTable!
+
+    // MARK: - Lifecycle
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        self.configureTableWithData(["Arran", "Bute", "Somewhere else"])
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    // MARK: - 
+    override func contextForSegueWithIdentifier(segueIdentifier: String, inTable table: WKInterfaceTable, rowIndex: Int) -> AnyObject? {
+        return "Test"
+    }
+    
+    // MARK: - Table config
+    func configureTableWithData(data: [String]) {
+        self.table.setNumberOfRows(data.count, withRowType: "serviceRow")
+        for var index = 0; index < data.count; index++ {
+            let rowData = data[index]
+            
+            let row = self.table.rowControllerAtIndex(index) as! ServiceRow
+            row.serviceLabel.setText(rowData)
+            row.serviceStatusImage.setImageNamed("green")
+        }
+    }
+
+}
---
+++ Ferries WatchKit Extension/ServiceRow.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceRow.swift
+//  FerryServices_2
+//
+//  Created by Stefan Church on 23/05/15.
+//  Copyright (c) 2015 Stefan Church. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class ServiceRow: NSObject {
+    
+    @IBOutlet weak var serviceStatusImage: WKInterfaceImage!
+    @IBOutlet weak var serviceLabel: WKInterfaceLabel!
+
+}
