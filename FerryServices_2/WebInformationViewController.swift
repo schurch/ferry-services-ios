608c64243c8c02926e5f49d54ee46ec1002f6da3
Stefan Church <stefan.church@gmail.com>
2016-07-17 11:11:21 +1200

    Fixed build; fixed handling of links in disruption information.

--- FerryServices_2/WebInformationViewController.swift
+++ FerryServices_2/WebInformationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class WebInformationViewController: UIViewController {
     
@@ -17,9 +18,23 @@ class WebInformationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        webView.delegate = self
+        
         if let html = self.html {
             let styledHtml = "<style type='text/css'>body {font: normal 14px HelveticaNeue; color: #555555;}</style>" + html
             webView.loadHTMLString(styledHtml, baseURL: nil)
         }
+    }
+}
+
+extension WebInformationViewController: UIWebViewDelegate {
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        guard navigationType == .LinkClicked else { return true }
+        guard let url = request.URL else { return true }
+        
+        let safariViewController = SFSafariViewController(URL: url)
+        presentViewController(safariViewController, animated: true, completion: nil)
+        
+        return false
     }
 }
--- Podfile
+++ Podfile
@@ -1,6 +1,8 @@
 platform :ios, '8.0'
 use_frameworks!
 
-pod 'FMDB', :git => 'https://github.com/robertmryan/fmdb.git', :branch => 'swiftFramework'
-pod 'Flurry-iOS-SDK/FlurrySDK'
-pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
+target 'FerryServices_2' do
+	pod 'FMDB'
+	pod 'Flurry-iOS-SDK/FlurrySDK'
+	pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
+end
