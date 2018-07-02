2a158821d981b7d05576deccec85859bad415ce9
Stefan Church <stefan.church@gmail.com>
2018-07-02 20:41:48 +1200

    Remove old winter timetables

--- FerryServices_2/ServiceDetailTableViewController.swift
+++ FerryServices_2/ServiceDetailTableViewController.swift
@@ -545,7 +545,7 @@ class ServiceDetailTableViewController: UIViewController {
     }
 
     fileprivate func winterPath() -> String {
-        return (Bundle.main.bundlePath as NSString).appendingPathComponent("Timetables/2017/Winter/\(serviceStatus.serviceId!).pdf")
+        return (Bundle.main.bundlePath as NSString).appendingPathComponent("Timetables/2018/Winter/\(serviceStatus.serviceId!).pdf")
     }
     
     fileprivate func summerPath() -> String {
