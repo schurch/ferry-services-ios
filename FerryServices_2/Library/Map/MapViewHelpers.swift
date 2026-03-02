d08fc7eb52d71169952e9ee2c4bfff0e4ef2d11c
Stefan Church <stefan.church@gmail.com>
2026-03-03 10:56:56 +1300

    Delete dead code

--- FerryServices_2/Library/Map/MapViewHelpers.swift
+++ FerryServices_2/Library/Map/MapViewHelpers.swift
@@ -10,61 +10,6 @@ import Foundation
 import MapKit
 
 class MapViewHelpers {
-    private class VesselAnnotationView: MKAnnotationView {
-        private var observation: NSKeyValueObservation?
-        
-        override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
-            super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
-            setup()
-        }
-        
-        required init?(coder aDecoder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        private func setup() {
-            guard let vesselAnnotation = annotation as? VesselAnnotation else { return }
-            observation = vesselAnnotation.observe(\.course, options: .new) { [weak self] object, change in
-                guard let newCourse = change.newValue else { return }
-                Task { @MainActor in
-                    self?.image = UIImage(named: "ferry")!.rotated(by: newCourse)
-                }
-            }
-        }
-    }
-    
-    @MainActor
-    static func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        switch annotation {
-        case let vesselAnnotation as VesselAnnotation:
-            let ferryView = mapView.dequeueReusableAnnotationView(withIdentifier: "ferry") ?? VesselAnnotationView(annotation: vesselAnnotation, reuseIdentifier: "ferry")
-            ferryView.annotation = vesselAnnotation
-            ferryView.displayPriority = .required
-            ferryView.canShowCallout = true
-            ferryView.image = UIImage(named: "ferry")!.rotated(by: vesselAnnotation.course)
-            
-            return ferryView
-            
-        case let locationAnnotation as LocationAnnotation:
-            let locationView = mapView.dequeueReusableAnnotationView(withIdentifier: "location") ?? MKMarkerAnnotationView(annotation: locationAnnotation, reuseIdentifier: "location")
-            locationView.annotation = locationAnnotation
-            locationView.displayPriority = .required
-            locationView.canShowCallout = true
-            
-            let directionsButton = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 100))
-            directionsButton.backgroundColor = UIColor(red:0.13, green:0.75, blue:0.67, alpha:1)
-            directionsButton.setImage(UIImage(named: "directions_arrow"), for: UIControl.State())
-            directionsButton.setImage(UIImage(named: "directions_arrow_highlighted"), for: UIControl.State.highlighted)
-            directionsButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 56, right: 0)
-            locationView.rightCalloutAccessoryView = directionsButton
-            
-            return locationView
-            
-        default :
-            return nil
-        }
-    }
-    
     static func calculateMapRect(forLocations locations: [Service.Location]) -> MKMapRect {
         let rect = locations.reduce(MKMapRect.null) { rect, location in
             rect.union(
